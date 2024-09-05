2c377f82e0916bd30e419361d369727dfda800d2
Cameron Nagle <154038638+CamNagle24@users.noreply.github.com>
2024-09-05 18:39:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    const menu = document.querySelection(".menu-links");
-    const icon = document.querySelection(".hamburger-icon");
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
